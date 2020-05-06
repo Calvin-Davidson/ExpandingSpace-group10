25ffcc9f69a3f5ccc2b50e0494e87568f641b5f6
Jason Dikken <electronicjason.dikken@gmail.com>
2020-05-06 13:54:25 +0200

    hover script

---
+++ Assets/Scripts/HoverScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverScript : MonoBehaviour
+{
+    public float horizontalSpeed;
+    public float verticalSpeed = 0.5f;
+    public float amplitude = 0.1f;
+
+    public Vector2 tempPosition;
+
+    private void Start()
+    {
+        tempPosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        tempPosition.x += horizontalSpeed;
+        tempPosition.y += Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
+        transform.position = tempPosition;
+    }
+}
+
